4983acd5aa12f8e23d83e01ee887e37992deefbc
ruiw2006 <ruiw2006@126.com>
2020-10-08 18:34:39 +0800

    change image pos&scales(adapt to 1920*1080 resolution)

---
+++ Assets/Scripts/WinLoseCondition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinLoseCondition : MonoBehaviour
+{
+    private List<GameObject> treesList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        treesList = TreesController.instance.GetTreesList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(treesList.Count);
+    }
+
+    void LoseCondition() 
+    { 
+        
+    }
+}
